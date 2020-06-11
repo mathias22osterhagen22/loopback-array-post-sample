f79211ab28889252d70d1c8036c95aa7b0924a54
lschweitzer <lucas.schweitzer@i-hub.com>
2020-06-11 14:49:23 +0200

    First commit

---
+++ src/models/people.model.ts
@@ -0,0 +1,28 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class People extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+
+  constructor(data?: Partial<People>) {
+    super(data);
+  }
+}
+
+export interface PeopleRelations {
+  // describe navigational properties here
+}
+
+export type PeopleWithRelations = People & PeopleRelations;
